14e13422479e940abd833d78b182aeee6f2e0614
Abdul <qkader03@gmail.com>
2024-06-12 14:56:11 -0400

    Added home page to start development from there

---
+++ webapp/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import login from './Features/login';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          DearbornSalvageAuto
+        </p>
+
+        <img src = "https://images.pexels.com/photos/3764984/pexels-photo-3764984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className='logo' ></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
