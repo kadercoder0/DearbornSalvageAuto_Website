83e2a5b15d7f9b11a47a7bde6888c153f57316c2
Atef Nasser <atefnass@umich.edu>
2024-06-18 14:30:33 -0400

    Created Register Page
    Created form to create account, linked it where you can easily switch pages between Login and Register page

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Login from './Features/LoginSignup/login';
+import Register from './Features/LoginSignup/register';
 import Home from './Features/home';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect to login */}
         </Routes>
