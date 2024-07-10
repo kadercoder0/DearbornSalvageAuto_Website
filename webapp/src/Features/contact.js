d643ad8564eb657c638012ce93d66f0d6c77e564
Esam Alwaseem <esamalw@umich.edu>
2024-07-10 13:08:44 -0400

    Added contact information

--- webapp/src/Features/contact.js
+++ webapp/src/Features/contact.js
@@ -1,13 +1,23 @@
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 const Contact = () => {
   return (
     <div>
       <h1>Contact Us</h1>
-      <p>Contact details and form.</p>
+      
+      {/* Contact details */}
+      <div>
+        <p><strong>Phone number:</strong> +13132036018</p>
+        <p><strong>Shop email:</strong> <a href="mailto:ahmadail.usedcars@gmail.com">ahmadail.usedcars@gmail.com</a></p>
+        <p><strong>Shop address:</strong> 14041 Greenfield Rd, Detroit, MI, United States</p>
+        <p><strong>Shop name:</strong> Greenfield Auto Center</p>
+      </div>
+
+      
+      {/* Navigation back to Home */}
       <nav className="navbar">
-        <Link to="/home" className="nav-link"> Back to Home</Link>
+        <Link to="/home" className="nav-link">Back to Home</Link>
       </nav>
     </div>
   );
