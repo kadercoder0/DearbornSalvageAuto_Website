7eba7beedab32ddad7150cefd1807a264beb80db
abdul <akqayed@umich.edu>
2024-08-21 11:04:49 -0400

    removed car details

--- webapp/src/Features/Inventory/CarList.js
+++ webapp/src/Features/Inventory/CarList.js
@@ -20,23 +20,6 @@ const CarList = ({ cars }) => {
                     <p>Price: ${car.price}</p>
                 </div>
             ))}
-
-            {selectedCar && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <span className="close" onClick={closeModal}>&times;</span>
-                        <img src={selectedCar.imageUrl} alt={`${selectedCar.make} ${selectedCar.model}`} className="modal-image" />
-                        <h2>{selectedCar.year} {selectedCar.make} {selectedCar.model}</h2>
-                        <p>Price: ${selectedCar.price}</p>
-                        <p>Description: {selectedCar.description}</p>
-                        <p>Drivetrain: {selectedCar.drivetrain}</p>
-                        <p>Engine Size: {selectedCar.engineSize}</p>
-                        <p>Odometer: {selectedCar.odometer} miles</p>
-                        <p>Title Status: {selectedCar.titleStatus}</p>
-                        <p>Cylinders: {selectedCar.cylinders}</p>
-                    </div>
-                </div>
-            )}
         </div>
     );
 };
