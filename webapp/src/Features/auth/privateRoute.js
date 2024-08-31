3f25b54f75bed80f3596eac2f87b7a242d8bb25a
MohammedHaidous <Mohammh@umich.edu>
2024-08-30 23:47:19 -0400

    Fixed defect in application that messed up the routing for the admin

--- webapp/src/Features/auth/privateRoute.js
+++ webapp/src/Features/auth/privateRoute.js
@@ -1,39 +1,19 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
-import { auth, db } from '../../firebase';
-import { getDoc, doc } from 'firebase/firestore';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../auth/authContext';
 
 const PrivateRoute = ({ children, adminOnly, ...rest }) => {
-  const [isAllowed, setIsAllowed] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
+  const { currentUser, isAdmin } = useAuth();
 
-  React.useEffect(() => {
-    const checkAuth = async () => {
-      const user = auth.currentUser;
-      if (user) {
-        if (adminOnly) {
-          // Check if the user is an admin
-          const docRef = doc(db, 'admin', 'privileges');
-          const docSnap = await getDoc(docRef);
-          if (docSnap.exists()) {
-            const privileges = docSnap.data();
-            if (privileges.uid === user.uid) {
-              setIsAllowed(true);
-            }
-          }
-        } else {
-          setIsAllowed(true);
-        }
-      }
-      setLoading(false);
-    };
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
 
-    checkAuth();
-  }, [adminOnly]);
+  if (adminOnly && !isAdmin) {
+    return <Navigate to="/home" />;
+  }
 
-  if (loading) return <p>Loading...</p>;
-
-  return isAllowed ? children : <Navigate to="/login" />;
+  return children;
 };
 
 export default PrivateRoute;
