be19b2fa43b5b32a4b80a74e337d146e8a610fa3
Abdul <abdul@gmail.com>
2024-11-15 20:41:04 -0500

    remove old home page, caused error

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Login from './Features/auth/LoginSignup/login';
 import Signup from './Features/auth/LoginSignup/signup';
-import Home from './Features/user/home';
 import About from './Features/user/about';
 import Inventory from './Features/user/Inventory/inventory';
 import Contact from './Features/user/contact';
@@ -33,7 +32,6 @@ function App() {
         <div className="App">
           <Routes>
             {/* Public Routes */}
-            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/inventory" element={<Inventory />} />
             <Route path="/inventory/car/:carId" element={<CarDetailsPage />} />
